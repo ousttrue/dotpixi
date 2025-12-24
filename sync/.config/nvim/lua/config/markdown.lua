1ec9ada08ab025ec67aef4137d067913d855fae3
ousttrue <ousttrue@gmail.com>
2025-12-24 16:23:50 +0900

    tmux

--- sync/.config/nvim/lua/config/markdown.lua
+++ sync/.config/nvim/lua/config/markdown.lua
@@ -3,14 +3,16 @@ return {
     --
     -- for docusaurus sidebar.ts
     --
-    vim.api.nvim_create_user_command("GfPrefix", function(params)
+    vim.api.nvim_create_user_command("GfPrefix", function(
+        params ---@diagnostic disable-line: unused-local
+    )
       vim.keymap.set("n", "gf", function()
         local dir = vim.fn.expand "%:p:h"
         local file = vim.fn.expand "<cfile>"
         if file:find "/$" then
           file = file:sub(1, #file - 1)
         end
-        print(dir, file)
+        print('markdown', 'GfPrefix', dir, file)
         local path = ("%s/docs/%s.md"):format(dir, file)
         local path_vitepress = ("%s/../docs/%s.md"):format(dir, file)
         if vim.uv.fs_stat(path) then
--- sync/.config/nvim/lua/plugins/git.lua
+++ sync/.config/nvim/lua/plugins/git.lua
@@ -142,7 +142,7 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-      { "<C-t>", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      { "<C-g>", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
   },
   {
