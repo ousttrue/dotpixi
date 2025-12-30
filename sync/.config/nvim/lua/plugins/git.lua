7909526b046039ae5dca5eb7df5067a57b358502
ousttrue <ousttrue@gmail.com>
2025-12-30 13:25:23 +0900

    <C-a>

--- sync/.config/nvim/lua/plugins/git.lua
+++ sync/.config/nvim/lua/plugins/git.lua
@@ -142,7 +142,7 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-      { "<Space>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      { "<C-a>", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
   },
   {
